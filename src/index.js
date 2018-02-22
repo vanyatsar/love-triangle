814a7a1297791022777515d2e87cbcce29f080d0
Ivan Tsar <ivan.tsar99@gmail.com>
2018-02-22 15:52:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
 for(var i = 0;i < preferences.length; i++){
  if (preferences[i]!=0){
 var n1 = preferences[i], n2=preferences[n1-1], n3 = preferences[n2-1];
- if((i+1 == n3) && (preferences[i] != 0) && (n1 != n3) && (n1 != n3) && (n2 != n3)){
+ if((i+1 == n3) && (preferences[i] != 0) && (n1 != n3) && (n1 != n2) && (n2 != n3)){
       ++count;
       preferences[i] = preferences[n1-1] = preferences[n2-1] = 0;
     }
