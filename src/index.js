f6bd88e777b064f83ae7cfc7c2479852143d3213
Ivan Tsar <ivan.tsar99@gmail.com>
2018-02-22 15:49:56 +0300

    second solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ var count = 0;
+for(var i = 0;i < preferences.length; i++){
+ if (preferences[i]!=0){
+var n1 = preferences[i], n2=preferences[n1-1], n3 = preferences[n2-1];
+ if((i+1 == n3) && (preferences[i] != 0) && (n1 != n3) && (n1 != n3) && (n2 != n3)){
+      ++count;
+      preferences[i] = preferences[n1-1] = preferences[n2-1] = 0;
+    }
+  }
+}
+  return count;
 };
+
+// var a = [2,3,1];
+// for (var j=0;j<a.length;j++){
+//   for(var i=0;i<a.length;i++){
+//     if(a[i] == a[i+1]){
+//     a.splice(i+1, 1);}
+//     else continue;
+// }
+// }
+//   console.log(a);
